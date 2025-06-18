40b7d062ad7a27f5b0ee370a1607a481a4438e1b
Cris <pachecocruzc258@gmail.com>
2025-06-17 22:08:58 -0400

    Updates methods on IRoomRepository

--- src/SGRH.Application/Interfaces/Repositories/Hotel/IRoomRepository.cs
+++ src/SGRH.Application/Interfaces/Repositories/Hotel/IRoomRepository.cs
@@ -1,9 +1,20 @@
-﻿using SGRH.Domain.Entities.Hotel;
+﻿using SGRH.Application.Dtos.Hotel.Room;
+using SGRH.Domain.Base;
 
 namespace SGRH.Application.Interfaces.Repositories.Hotel
 {
-    public interface IRoomRepository : IBaseRepository<Room>
+    public interface IRoomRepository 
     {
-        //...
+        //Arreglados, faltan Dtos 
+
+        Task<OperationResult> GetAllAsync();
+        Task<OperationResult> GetByIdAsync(int id);
+        Task<OperationResult> AddAsync(CreateRoomDto createRoomDto);
+        Task<OperationResult> UpdateAsync(ModifyRoomDto modifyRoomDto);
+        Task<OperationResult> DeleteAsync(DisableRoomDto disableRoomDto);
+        
+        //Task<OperationResult> GetAllAsync(Expression<Func<TEntity, bool>> filter);
+        //Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter);
+
     }
 }
