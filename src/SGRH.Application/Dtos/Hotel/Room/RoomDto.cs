f91f8b39030ee8b021a0844b7f20f5787202d718
Cris <pachecocruzc258@gmail.com>
2025-06-17 22:19:15 -0400

    Create Dtos folder and add Hotel and Report Dtos

---
+++ src/SGRH.Application/Dtos/Hotel/Room/RoomDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGRH.Application.Dtos.Hotel.Room
+{
+    public record RoomDto
+    {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public int CategoryId { get; set; }
+        public int FloorId { get; set; }
+        public string? Description { get; set; }
+        public string? RoomImgUrl { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ src/SGRH.Application/Dtos/Report/ServiceRevenueReportDto.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SGRH.Application.Dtos.Report
+{
+    public record ServiceRevenueReportDto
+    {
+        public string ServiceName { get; set; }
+        public string CategoryName { get; set; }
+        public long ServiceCount  { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public decimal AvgRevenuePerService { get; set; }
+
+    }
+}
