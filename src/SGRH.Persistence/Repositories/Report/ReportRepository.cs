73e65f896353e49604a596fe8ed9751474b02339
Erkyhanma <yenzel.bt@gmail.com>
2025-06-19 18:16:56 -0400

    Refactor StoreProcedureEx method for better readability

--- src/SGRH.Persistence/Repositories/Report/ReportRepository.cs
+++ src/SGRH.Persistence/Repositories/Report/ReportRepository.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SGRH.Persistence.Repositories
+namespace SGRH.Persistence.Repositories.Report
 {
     public class ReportRepository : IReportRepository
     {
--- src/SGRH.Persistence/Repositories/ReservationModule/ReservationRepository.cs
+++ src/SGRH.Persistence/Repositories/ReservationModule/ReservationRepository.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
-using Npgsql;
 using SGRH.Application.Dtos.ReservationModule.Reservation;
 using SGRH.Application.Interfaces.Repositories.ReservationModule;
 using SGRH.Domain.Base;
-using System.Data;
+using SGRH.Persistence.Helpers;
 
 namespace SGRH.Persistence.Repositories.ReservationModule
 {
@@ -29,64 +28,38 @@ namespace SGRH.Persistence.Repositories.ReservationModule
         public async Task<OperationResult<CreateReservationDto>> AddAsync(CreateReservationDto createReservationDto)
         {
 
-            var pResult = new OperationResult<CreateReservationDto>();
+            _logger.LogInformation($"Creating Reservation for client {createReservationDto.ClientId}");
 
-            try
+            var parameters = new Dictionary<string, object>
             {
-                _logger.LogInformation($"Creating reservation number {createReservationDto.ClientId}");
+                {"p_client_id", createReservationDto.ClientId },
+                { "p_room_id", createReservationDto.RoomId },
+                { "p_start_date", createReservationDto.StartDate },
+                { "p_end_date", createReservationDto.EndDate },
+                { "p_status", createReservationDto.Status },
+                { "p_guest_count",createReservationDto.GuestCount },
+                { "p_created_by", createReservationDto.CreatedBy }
+            };
 
-                using (var context = new NpgsqlConnection(_connectionString))
-                {
-                    using (var command = new NpgsqlCommand("reservationModule.CreateReservation", context))
-                    {
+            var StoredProcedureResult = await StoreProcedureEx.ExecuteAsync(
+                _connectionString,
+                "reservationModule.CreateReservation",
+                parameters,
+                _logger
+            );
 
-                        command.CommandType = System.Data.CommandType.StoredProcedure;
-                        command.Parameters.AddWithValue("p_client_id", createReservationDto.ClientId);
-                        command.Parameters.AddWithValue("p_room_id", createReservationDto.RoomId);
-                        command.Parameters.AddWithValue("p_start_date", createReservationDto.StartDate);
-                        command.Parameters.AddWithValue("p_end_date", createReservationDto.EndDate);
-                        command.Parameters.AddWithValue("p_status", createReservationDto.Status);
-                        command.Parameters.AddWithValue("p_guest_count", createReservationDto.GuestCount);
-                        command.Parameters.AddWithValue("p_created_by", createReservationDto.CreatedBy);
-
-
-                        var outputParam = new NpgsqlParameter("presult", NpgsqlTypes.NpgsqlDbType.Text)
-                        {
-                            Direction = ParameterDirection.Output
-                        };
-
-                        command.Parameters.Add(outputParam);
-
-                        await context.OpenAsync();
-                        var result = await command.ExecuteNonQueryAsync();
-
-                        string message = outputParam.Value?.ToString() ?? "No message returned.";
-
-
-                        if (result > 0)
-                        {
-                            pResult = OperationResult<CreateReservationDto>.Success(message, createReservationDto);
-                            _logger.LogInformation($"Reservation added successfully", createReservationDto);
-                        }
-                        else
-                        {
-                            pResult = OperationResult<CreateReservationDto>.Failure(message);
-                            _logger.LogError($"Failure to add reservation");
-                        }
-                    }
-                }
-
-            }
-            catch (Exception ex)
+            if (StoredProcedureResult.IsSuccess)
             {
-                _logger.LogError(ex, "Error adding the reservation");
-                return OperationResult<CreateReservationDto>.Failure("An error ocurred while adding reservation");
-
+                return OperationResult<CreateReservationDto>.Success(StoredProcedureResult.Message, createReservationDto);
             }
-            return pResult;
-
-
+            else
+            {
+                return OperationResult<CreateReservationDto>.Failure(StoredProcedureResult.Message);
+            }
         }
+
+
+
         public async Task<OperationResult<UpdateReservationDto>> UpdateAsync(UpdateReservationDto updateReservationDto)
         {
             throw new NotImplementedException();
