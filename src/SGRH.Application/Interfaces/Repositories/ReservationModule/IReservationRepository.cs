cad366eb8e9c9b44580a83f6c49302f26c16f4e1
Erkyhanma <yenzel.bt@gmail.com>
2025-06-19 00:52:25 -0400

    Add methods for reservationModule repository interfaces

--- src/SGRH.Application/Interfaces/Repositories/ReservationModule/IReservationRepository.cs
+++ src/SGRH.Application/Interfaces/Repositories/ReservationModule/IReservationRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace SGRH.Application.Interfaces.Repositories.ReservationModule
+﻿using SGRH.Application.Dtos.ReservationModule.Reservation;
+using SGRH.Domain.Base;
+
+namespace SGRH.Application.Interfaces.Repositories.ReservationModule
 {
     public interface IReservationRepository
     {
-
+        Task<OperationResult> GetAllAsync();
+        Task<OperationResult> GetByIdAsync(int id);
+        Task<OperationResult> AddAsync(CreateReservationDto createReservationDto);
+        Task<OperationResult> UpdateAsync(UpdateReservationDto updateReservationDto);
+        Task<OperationResult> DeleteAsync(DisableReservationDto disableReservationDto);
+        Task<OperationResult> CheckAvailability(CheckRoomAvailabilityResultDto checkRoomAvailabilityResultDto);
     }
 }
--- src/SGRH.Application/Interfaces/Repositories/ReservationModule/IReservationServiceRepository.cs
+++ src/SGRH.Application/Interfaces/Repositories/ReservationModule/IReservationServiceRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace SGRH.Application.Interfaces.Repositories.ReservationModule
+﻿using SGRH.Application.Dtos.ReservationModule.ReservationService;
+using SGRH.Domain.Base;
+
+namespace SGRH.Application.Interfaces.Repositories.ReservationModule
 {
     public interface IReservationServiceRepository
     {
+        public Task<OperationResult> AddAsync(CreateReservationServiceDto createReservationServiceDto);
+        public Task<OperationResult> DeleteAsync(DeleteReservationServiceDto deleteReservationServiceDto);
+
     }
 }
