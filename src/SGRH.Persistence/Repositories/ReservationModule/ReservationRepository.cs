efc14ea14792bdd084a6093c5e944350d0534d32
Erkyhanma <yenzel.bt@gmail.com>
2025-06-20 16:01:42 -0400

    Add Service Repository

--- src/SGRH.Persistence/Repositories/ReservationModule/ReservationRepository.cs
+++ src/SGRH.Persistence/Repositories/ReservationModule/ReservationRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Npgsql;
 using SGRH.Application.Dtos.ReservationModule.Reservation;
 using SGRH.Application.Interfaces.Repositories.ReservationModule;
 using SGRH.Domain.Base;
@@ -19,11 +20,109 @@ namespace SGRH.Persistence.Repositories.ReservationModule
 
         public async Task<OperationResult<IEnumerable<ReservationDto>>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var reservations = new List<ReservationDto>();
+            try
+            {
+                _logger.LogInformation("Getting all Reservations");
+
+                using (var context = new NpgsqlConnection(_connectionString))
+                {
+                    await context.OpenAsync();
+
+                    using (var command = new NpgsqlCommand("SELECT * FROM reservationModule.GetAllReservationsWithServices", context))
+                    {
+                        using var reader = await command.ExecuteReaderAsync();
+
+                        while (await reader.ReadAsync())
+                        {
+                            var reservation = new ReservationDto
+                            {
+                                ReservationId = reader.GetInt32(0),
+                                ClientId = reader.GetInt32(1),
+                                RoomId = reader.GetInt32(2),
+                                StartDate = reader.GetDateTime(3),
+                                EndDate = reader.GetDateTime(4),
+                                ReservationDate = reader.GetDateTime(5),
+                                Status = reader.GetString(6),
+                                GuestCount = reader.GetInt32(7),
+                                PaymentAmount = reader.GetDecimal(8),
+                                ServicesCount = reader.GetInt32(9),
+                                TotalServicesCost = reader.GetDecimal(10),
+                                ServiceNames = reader.IsDBNull(11) ? "" : reader.GetString(11),
+                                CreatedAt = reader.GetDateTime(12),
+                                UpdatedAt = reader.GetDateTime(13)
+                            };
+
+                            reservations.Add(reservation);
+                        }
+                    }
+                }
+
+                if (reservations.Count == 0)
+                {
+                    return OperationResult<IEnumerable<ReservationDto>>.Failure("No reservations found");
+                }
+
+                return OperationResult<IEnumerable<ReservationDto>>.Success("Reservations retrieved successfully", reservations);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving reservations");
+                return OperationResult<IEnumerable<ReservationDto>>.Failure("Error retrieving reservations");
+            }
         }
         public async Task<OperationResult<ReservationDto>> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _logger.LogInformation($"Getting reservation {id}");
+
+                using (var context = new NpgsqlConnection(_connectionString))
+                {
+                    await context.OpenAsync();
+
+                    using (var command = new NpgsqlCommand("SELECT * FROM reservationModule.GetReservationByID(@p_reservationId)", context))
+                    {
+                        command.Parameters.AddWithValue("@p_reservationId", id);
+
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                var reservation = new ReservationDto
+                                {
+                                    ReservationId = reader.GetInt32(reader.GetOrdinal("reservation_id")),
+                                    ClientId = reader.GetInt32(reader.GetOrdinal("client_id")),
+                                    RoomId = reader.GetInt32(reader.GetOrdinal("room_id")),
+                                    StartDate = reader.GetDateTime(reader.GetOrdinal("start_date")),
+                                    EndDate = reader.GetDateTime(reader.GetOrdinal("end_date")),
+                                    ReservationDate = reader.GetDateTime(reader.GetOrdinal("reservation_date")),
+                                    Status = reader.GetString(reader.GetOrdinal("status")),
+                                    GuestCount = reader.GetInt32(reader.GetOrdinal("guest_count")),
+                                    PaymentAmount = reader.GetDecimal(reader.GetOrdinal("payment_amount")),
+                                    ServicesCount = reader.GetInt32(reader.GetOrdinal("services_count")),
+                                    TotalServicesCost = reader.GetDecimal(reader.GetOrdinal("total_services_cost")),
+                                    ServiceNames = reader.IsDBNull(reader.GetOrdinal("service_names")) ? "" : reader.GetString(reader.GetOrdinal("service_names")),
+                                    CreatedAt = reader.GetDateTime(reader.GetOrdinal("created_at")),
+                                    UpdatedAt = reader.GetDateTime(reader.GetOrdinal("updated_at")),
+
+                                };
+
+
+
+                                return OperationResult<ReservationDto>.Success("Reservation retrieved successfully", reservation);
+                            }
+                        }
+                    }
+                }
+
+                return OperationResult<ReservationDto>.Failure("Reservation not found");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while retrieving reservation");
+                return OperationResult<ReservationDto>.Failure("Unable to retrieve data");
+            }
         }
         public async Task<OperationResult<CreateReservationDto>> AddAsync(CreateReservationDto createReservationDto)
         {
@@ -57,20 +156,107 @@ namespace SGRH.Persistence.Repositories.ReservationModule
                 return OperationResult<CreateReservationDto>.Failure(StoredProcedureResult.Message);
             }
         }
-
-
-
         public async Task<OperationResult<UpdateReservationDto>> UpdateAsync(UpdateReservationDto updateReservationDto)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"Update reservation {updateReservationDto.ReservationId}");
+
+            var parameters = new Dictionary<string, object>()
+            {
+                { "p_reservation_id", updateReservationDto.ReservationId},
+                { "p_client_id", updateReservationDto.ReservationId},
+                { "p_room_id", updateReservationDto.ReservationId},
+                { "p_start_date", updateReservationDto.ReservationId},
+                { "p_end_date", updateReservationDto.ReservationId},
+                { "p_status", updateReservationDto.ReservationId},
+                { "p_guest_count", updateReservationDto.ReservationId},
+                { "p_updated_by", updateReservationDto.ReservationId},
+            };
+
+            var StoredProcedureResult = await StoreProcedureEx.ExecuteAsync(
+             _connectionString,
+             "reservationModule.CreateReservation",
+             parameters,
+             _logger
+            );
+
+            if (StoredProcedureResult.IsSuccess)
+            {
+                return OperationResult<UpdateReservationDto>.Success(StoredProcedureResult.Message, updateReservationDto);
+            }
+            else
+            {
+                return OperationResult<UpdateReservationDto>.Failure(StoredProcedureResult.Message);
+            }
         }
         public async Task<OperationResult<DisableReservationDto>> DeleteAsync(DisableReservationDto disableReservationDto)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"Disable reservation {disableReservationDto.ReservationId}");
+
+            var parameters = new Dictionary<string, object>()
+            {
+                {"p_reservation_id", disableReservationDto.ReservationId},
+                {"p_updated_by", disableReservationDto.UpdatedBy},
+                {"p_deleted_by", disableReservationDto.DeleteBy},
+            };
+
+            var StoredProcedureResult = await StoreProcedureEx.ExecuteAsync(
+                _connectionString,
+                "reservationModule.DisableReservation",
+                parameters, _logger);
+
+            if (StoredProcedureResult.IsSuccess)
+            {
+                return OperationResult<DisableReservationDto>.Success(StoredProcedureResult.Message, disableReservationDto);
+
+            }
+            else
+            {
+                return OperationResult<DisableReservationDto>.Failure(StoredProcedureResult.Message);
+            }
+
+
         }
-        public async Task<OperationResult<CheckRoomAvailabilityResultDto>> CheckAvailability(CheckRoomAvailabilityResultDto checkRoomAvailabilityResultDto)
+        public async Task<OperationResult<CheckRoomAvailabilityResultDto>> CheckAvailability(int RoomId, DateTime StartDate, DateTime EndDate)
         {
-            throw new NotImplementedException();
+
+            try
+            {
+                _logger.LogInformation($"Checking room {RoomId}");
+
+                using (var context = new NpgsqlConnection(_connectionString))
+                {
+                    await context.OpenAsync();
+
+                    using (var command = new NpgsqlCommand("SELECT * from reservationModule.CheckRoomAvailability(@p_room_id, @p_start_date, @p_end_date)", context))
+                    {
+                        command.Parameters.AddWithValue("@p_room_id", RoomId);
+                        command.Parameters.AddWithValue("@p_start_date", StartDate);
+                        command.Parameters.AddWithValue("@p_end_date", EndDate);
+
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                var availability = new CheckRoomAvailabilityResultDto
+                                {
+                                    IsAvailable = reader.GetBoolean(reader.GetOrdinal("is_available")),
+                                    Message = reader.GetString(reader.GetOrdinal("f_message"))
+                                };
+
+                                return OperationResult<CheckRoomAvailabilityResultDto>.Success(availability.Message, availability);
+                            }
+                        }
+                    }
+                }
+                return OperationResult<CheckRoomAvailabilityResultDto>.Failure($"The room {RoomId} was not found ");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while retrieving data");
+                return OperationResult<CheckRoomAvailabilityResultDto>.Failure("Unable to retrieve data");
+            }
+
+
         }
     }
 }
---
+++ src/SGRH.Persistence/Repositories/ReservationModule/ReservationServiceRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Logging;
+using SGRH.Application.Dtos.ReservationModule.ReservationService;
+using SGRH.Application.Interfaces.Repositories.ReservationModule;
+using SGRH.Domain.Base;
+using SGRH.Persistence.Helpers;
+
+
+namespace SGRH.Persistence.Repositories.ReservationModule
+{
+    public class ReservationServiceRepository : IReservationServiceRepository
+    {
+
+        private readonly string _connectionString;
+        private readonly ILogger<ReservationRepository> _logger;
+
+        public ReservationServiceRepository(string connectionString, ILogger<ReservationRepository> logger)
+        {
+            _connectionString = connectionString;
+            _logger = logger;
+        }
+
+        public async Task<OperationResult<CreateReservationServiceDto>> AddAsync(CreateReservationServiceDto createReservationServiceDto)
+        {
+            _logger.LogInformation("Add service for a reservation");
+
+            var parameters = new Dictionary<string, object>()
+            {
+                {"p_reservation_id", createReservationServiceDto.ReservationId },
+                {"p_service_id", createReservationServiceDto.ServiceId }
+            };
+
+            var StoreProcedureResult = await StoreProcedureEx.ExecuteAsync(
+                _connectionString,
+                "reservationModule.AddServiceReservation",
+                parameters,
+                _logger);
+
+            if (StoreProcedureResult.IsSuccess)
+            {
+                return OperationResult<CreateReservationServiceDto>.Success(StoreProcedureResult.Message, createReservationServiceDto);
+            }
+            else
+            {
+                return OperationResult<CreateReservationServiceDto>.Failure(StoreProcedureResult.Message);
+            }
+
+        }
+
+        public async Task<OperationResult<DeleteReservationServiceDto>> DeleteAsync(DeleteReservationServiceDto deleteReservationServiceDto)
+        {
+            _logger.LogInformation("Remove service for a reservation");
+
+            var parameters = new Dictionary<string, object>()
+            {
+                {"p_reservation_id", deleteReservationServiceDto.ReservationId },
+                {"p_service_id", deleteReservationServiceDto.ServiceId }
+            };
+
+            var StoreProcedureResult = await StoreProcedureEx.ExecuteAsync(
+                _connectionString,
+                "reservationModule.DeleteServiceReservation",
+                parameters,
+                _logger);
+
+            if (StoreProcedureResult.IsSuccess)
+            {
+                return OperationResult<DeleteReservationServiceDto>.Success(StoreProcedureResult.Message, deleteReservationServiceDto);
+            }
+            else
+            {
+                return OperationResult<DeleteReservationServiceDto>.Failure(StoreProcedureResult.Message);
+            }
+
+        }
+    }
+}
