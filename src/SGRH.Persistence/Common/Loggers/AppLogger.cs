4c92350678392b2baaaf38915836135d1f1b98ef
Cris <pachecocruzc258@gmail.com>
2025-06-21 19:03:13 -0400

    Add interface IAppLogger, class AppLogger for logging

---
+++ src/SGRH.Persistence/Common/Loggers/AppLogger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using SGRH.Persistence.Common.Loggers.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace SGRH.Application.Common.Logging
+{
+    public class AppLogger<T> : IAppLogger<T> 
+    {
+        private readonly ILogger<T> _logger;
+
+        public AppLogger(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        // Registra error con excepcion, incluyendo su [Type] o tipo de clase
+        public void ErrorEx(Exception ex, string message, params object[] args)
+        {
+            _logger.LogError(ex, "[{Type}] " + message, typeof(T).Name, args);
+        }
+
+        // Registra error sin excepcion, incluyendo su [Type] o tipo de clase
+        public void ErrorNoEx(string message, params object[] args)
+        {
+            _logger.LogError("[{Type}] " + message, typeof(T).Name, args);
+        }
+
+        // Registra mensaje de informe, incluyendo su [Type] o tipo de clase
+        public void Info(string message, params object[] args)
+        {
+            _logger.LogInformation("[{Type}] " + message, typeof(T).Name, args);
+        }
+
+    }
+
+}
