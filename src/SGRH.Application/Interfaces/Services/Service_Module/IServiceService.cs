fb87873c094e967c6c39d49be2491a894fd8c8e0
Erkyhanma <yenzel.bt@gmail.com>
2025-06-25 19:00:04 -0400

    Add Service for the Service entity

--- src/SGRH.Application/Interfaces/Services/Service_Module/IServiceService.cs
+++ src/SGRH.Application/Interfaces/Services/Service_Module/IServiceService.cs
@@ -5,9 +5,9 @@ namespace SGRH.Application.Interfaces.Services.Service_Module
 {
     public interface IServiceService
     {
-        Task<OperationResult<ServiceDto>> GetAllServicesAsync();
+        Task<OperationResult<IEnumerable<ServiceDto>>> GetAllServicesAsync();
         Task<OperationResult<ServiceDto>> GetServiceByIdAsync(int id);
-        Task<OperationResult<ServiceDto>> CreateServicesAsync(CreateServiceDto createServiceDto);
+        Task<OperationResult<CreateServiceDto>> CreateServicesAsync(CreateServiceDto createServiceDto);
         Task<OperationResult<ServiceDto>> UpdateServicesAsync(ServiceDto serviceDto);
         Task<OperationResult<ServiceDto>> DeleteServicesAsync(ServiceDto serviceDto);
 
