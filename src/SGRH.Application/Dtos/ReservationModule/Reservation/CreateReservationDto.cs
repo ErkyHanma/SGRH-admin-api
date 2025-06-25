d4862bbb24722e8a877eb55f59290e0ea180a687
Erkyhanma <yenzel.bt@gmail.com>
2025-06-24 23:42:26 -0400

    Add ServiceModule and Reservation Module services interfaces

--- src/SGRH.Application/Dtos/ReservationModule/Reservation/CreateReservationDto.cs
+++ src/SGRH.Application/Dtos/ReservationModule/Reservation/CreateReservationDto.cs
@@ -5,10 +5,12 @@
     {
         public int ClientId { get; set; }
         public int RoomId { get; set; }
-        public DateOnly StartDate { get; set; } = new DateOnly();
-        public DateOnly EndDate { get; set; }
+        public DateTime StartDate { get; set; } = new DateTime();
+        public DateTime EndDate { get; set; }
         public string Status { get; set; }
         public int GuestCount { get; set; }
+        public decimal PaymentAmount { get; set; }
+
         public int CreatedBy { get; set; }
 
 
---
+++ src/SGRH.Application/Interfaces/Services/Service_Module/IServiceService.cs
@@ -0,0 +1,15 @@
+﻿using SGRH.Application.Dtos.ServiceModule;
+using SGRH.Domain.Base;
+
+namespace SGRH.Application.Interfaces.Services.Service_Module
+{
+    public interface IServiceService
+    {
+        Task<OperationResult<ServiceDto>> GetAllServicesAsync();
+        Task<OperationResult<ServiceDto>> GetServiceByIdAsync(int id);
+        Task<OperationResult<ServiceDto>> CreateServicesAsync(CreateServiceDto createServiceDto);
+        Task<OperationResult<ServiceDto>> UpdateServicesAsync(ServiceDto serviceDto);
+        Task<OperationResult<ServiceDto>> DeleteServicesAsync(ServiceDto serviceDto);
+
+    }
+}
