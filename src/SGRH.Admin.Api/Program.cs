fe9d7e43affa5f70f2be4b3421bcaea628e0f743
Cris <pachecocruzc258@gmail.com>
2025-06-25 22:07:45 -0400

    Added routes for Room and Report services

--- src/SGRH.Admin.Api/Program.cs
+++ src/SGRH.Admin.Api/Program.cs
@@ -1,23 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+using SGRH.Application.Interfaces.Repositories.Hotel;
+using SGRH.Application.Interfaces.Repositories.Report;
+using SGRH.Application.Interfaces.Services.Hotel;
+using SGRH.Application.Interfaces.Services.Report;
+using SGRH.Application.Services.Hotel;
+using SGRH.Application.Services.Report;
+using SGRH.Persistence.Repositories.Hotel;
+using SGRH.Persistence.Repositories.Report;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace SGRH.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //references
+    public class Program
+    {
+        //references
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            // Modulo de habitaciones
+
+            builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+            builder.Services.AddTransient<IRoomService, RoomService>();
+
+            // Modulo de Reportes
+
+            builder.Services.AddScoped<IReportRepository, ReportRepository>();
+            builder.Services.AddTransient<IReportService, ReportService>();
+
+            // Modulo Tarifa (esperando)
+
+            // ...
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+        }
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
