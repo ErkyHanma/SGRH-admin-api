4c5734d25ed8e04c100c55d2730719c898165b97
Cris <pachecocruzc258@gmail.com>
2025-06-28 19:35:39 -0400

    Minor changes to AuditEntity

--- src/SGRH.Domain/Base/AuditEntity.cs
+++ src/SGRH.Domain/Base/AuditEntity.cs
@@ -5,8 +5,8 @@
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public int CreatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
-        public int UpdatedBy { get; set; }
-        public int DeletedBy { get; set; }
+        public int? UpdatedBy { get; set; }
+        public int? DeletedBy { get; set; }
         public DateTime? DeleteAt { get; set; } // <----- there's a typo
         public bool IsDeleted { get; set; }
         public bool IsActive { get; set; } = true;
