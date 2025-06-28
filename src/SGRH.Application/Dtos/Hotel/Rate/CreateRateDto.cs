43e5d7c3bc478ca2b9dd6ed6c980ff94a87c0674
Cris <pachecocruzc258@gmail.com>
2025-06-28 19:30:10 -0400

    Adds folder Validators for Rate Module

---
+++ src/SGRH.Application/Dtos/Hotel/Rate/CreateRateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGRH.Application.Dtos.Hotel.Rate
+{
+    public record CreateRateDto
+    {
+        public int CategoryId { get; set; }
+        public int SeasonId { get; set; }
+        public decimal NightPrice { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ src/SGRH.Application/Dtos/Hotel/Rate/Validators/UpdateRateValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using SGRH.Application.Dtos.Hotel.Rate;
+
+public class UpdateRateValidator : AbstractValidator<UpdateRateDto>
+{
+    public UpdateRateValidator()
+    {
+        RuleFor(x => x.RateId)
+            .GreaterThan(0).WithMessage("RateId is required.");
+
+        RuleFor(x => x.CategoryId)
+            .GreaterThan(0).WithMessage("CategoryId must be greater than zero.");
+
+        RuleFor(x => x.SeasonId)
+            .GreaterThan(0).WithMessage("SeasonId must be greater than zero.");
+
+        RuleFor(x => x.NightPrice)
+            .GreaterThan(0).WithMessage("NightPrice must be greater than zero.");
+
+        RuleFor(x => x.UpdatedBy)
+            .GreaterThan(0).WithMessage("UpdatedBy must be greater than zero.");
+    }
+}
