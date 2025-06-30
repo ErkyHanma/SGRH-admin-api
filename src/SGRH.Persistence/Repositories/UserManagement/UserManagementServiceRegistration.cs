139ee90a47e04fc2d9f041d0cf392e0228e22f8c
AnderssonBeltre <anderssonbeltre@gmail.com>
2025-06-29 23:05:46 -0400

    Avance parcial: ClientsController integrado y conexión a PostgreSQL establecida (pendiente mejora SQL, ya que no logra usar la tabla Clients)

--- src/SGRH.Persistence/Repositories/UserManagement/UserManagementServiceRegistration.cs
+++ src/SGRH.Persistence/Repositories/UserManagement/UserManagementServiceRegistration.cs
@@ -1,13 +1,19 @@
 ﻿using Core.Application.Interfaces.Repositories.UserManagement;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SGRH.Persistence.Repositories.UserManagement;
 
 namespace SGRH.Persistence.Repositories.UserManagement
 {
     public static class UserManagementServiceRegistration
     {
-        public static IServiceCollection AddUserManagementRepositories(this IServiceCollection services)
+        public static IServiceCollection AddUserManagementRepositories(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddScoped<IClientRepository, ClientRepository>();
+            var connectionString = configuration.GetConnectionString("SGRH");
+
+            services.AddScoped<IClientRepository>(provider =>
+                new ClientRepository(connectionString));
+
             return services;
         }
     }
