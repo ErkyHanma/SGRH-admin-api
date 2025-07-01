e691b38172fe99843f5baea6e1f2b1c328bad5f0
Cris <pachecocruzc258@gmail.com>
2025-07-01 14:35:21 -0400

    Add validation logic on ReportService

--- src/SGRH.Application/Services/Report/ReportService.cs
+++ src/SGRH.Application/Services/Report/ReportService.cs
@@ -16,13 +16,13 @@ namespace SGRH.Application.Services.Report
 {
     public sealed class ReportService : IReportService
     {
-        private readonly IReportRepository _reportService;
+        private readonly IReportRepository _reportRepository;
         private readonly IAppLogger<ReportService> _logger;
         private readonly IConfiguration _configuration;
 
-        public ReportService(IReportRepository reportService, IAppLogger<ReportService> logger, IConfiguration configuration)
+        public ReportService(IReportRepository reportRepository, IAppLogger<ReportService> logger, IConfiguration configuration)
         {
-            _reportService = reportService;
+            _reportRepository = reportRepository;
             _logger = logger;
             _configuration = configuration;
         }
@@ -31,14 +31,15 @@ namespace SGRH.Application.Services.Report
         {
             try
             {
-                if (request == null)
+                var validation = ValidateDateRange(request);
+                if (!validation.IsSuccess)
                 {
-                    return OperationResult<IEnumerable<OcuppancyReportDto>>.Failure("Request is null for Occupancy Revenue Report.");
+                    return OperationResult<IEnumerable<OcuppancyReportDto>>.Failure(validation.Message);
                 }
 
                 _logger.Info("Getting an Occupancy Report from {Start} to {End}", request.StartDate, request.EndDate);
 
-                var operationResult = await _reportService.GetOcuppancyReportAsync(request);
+                var operationResult = await _reportRepository.GetOcuppancyReportAsync(request);
 
                 if (!operationResult.IsSuccess)
                 {
@@ -54,20 +55,21 @@ namespace SGRH.Application.Services.Report
                 _logger.ErrorEx(ex, "Error generating occupancy report."); // deberia estar en el archivo de configuracion?
                 return OperationResult<IEnumerable<OcuppancyReportDto>>.Failure($"An error occurred. {ex.Message}");
             }
-
-
         }
 
-        public async Task<OperationResult<IEnumerable<RatesReportDto>>> GetRatesReport(ReportDateRangeRequestDto request) // ARREGLAR
+        public async Task<OperationResult<IEnumerable<RatesReportDto>>> GetRatesReport(ReportDateRangeRequestDto request) 
         {
             try
             {
-                if (request == null)
-                    return OperationResult<IEnumerable<RatesReportDto>>.Failure("Request is null for Rates Report.");
+                var validation = ValidateDateRange(request);
+                if (!validation.IsSuccess)
+                {
+                    return OperationResult<IEnumerable<RatesReportDto>>.Failure(validation.Message);
+                }
 
                 _logger.Info("Getting a Rates Report from {Start} to {End}", request.StartDate, request.EndDate);
 
-                var operationResult = await _reportService.GetRatesReportAsync(request);
+                var operationResult = await _reportRepository.GetRatesReportAsync(request);
 
                 if (!operationResult.IsSuccess)
                 {
@@ -83,19 +85,19 @@ namespace SGRH.Application.Services.Report
             }
         }
 
-
         public async Task<OperationResult<IEnumerable<RevenueReportDto>>> GetRevenueReport(ReportDateRangeRequestDto request)
         {
             try
             {
-                if (request == null)
+                var validation = ValidateDateRange(request);
+                if (!validation.IsSuccess)
                 {
-                    return OperationResult<IEnumerable<RevenueReportDto>>.Failure("Request is null for Revenue Report.");
+                    return OperationResult<IEnumerable<RevenueReportDto>>.Failure(validation.Message);
                 }
 
                 _logger.Info("Getting a Revenue Report from {Start} to {End}", request.StartDate, request.EndDate);
 
-                var operationResult = await _reportService.GetRevenueReportAsync(request);
+                var operationResult = await _reportRepository.GetRevenueReportAsync(request);
 
 
                 if (!operationResult.IsSuccess)
@@ -116,8 +118,6 @@ namespace SGRH.Application.Services.Report
 
         public async Task<OperationResult<IEnumerable<ServiceRevenueReportDto>>> GetServiceRevenueReport(ServiceRevenueRequestDto request)
         {
-            OperationResult<IEnumerable<ServiceRevenueReportDto>> operationResult = new OperationResult<IEnumerable<ServiceRevenueReportDto>>();
-
             try
             {
                 if (request == null)
@@ -127,15 +127,13 @@ namespace SGRH.Application.Services.Report
 
                 _logger.Info("Getting Service Revenue Report for CategoryId: {0}", request.CategoryId.HasValue ? request.CategoryId.Value.ToString() : "All");
 
-                if (operationResult.IsSuccess)
+                var operationResult = await _reportRepository.GetServiceRevenueReportAsync(request);
+
+                if (!operationResult.IsSuccess)
                 {
                     _logger.ErrorNoEx($"An error has occured while getting a report: {operationResult.Message}.");
                     return operationResult;
                 }
-
-                operationResult = await _reportService.GetServiceRevenueReportAsync(request);
-
-                //...
 
                 return operationResult;
 
@@ -143,9 +141,23 @@ namespace SGRH.Application.Services.Report
             catch (Exception ex)
             {
                 _logger.ErrorEx(ex, "Error occurred while getting Service Revenue Report.");
-                operationResult = OperationResult<IEnumerable<ServiceRevenueReportDto>>.Failure("An error occurred while generating the service revenue report.");
+                return OperationResult<IEnumerable<ServiceRevenueReportDto>>.Failure("An error occurred while generating the service revenue report.");
             }
-            return operationResult;
+        }
+
+        // Validaciones
+        private OperationResult<string> ValidateDateRange(ReportDateRangeRequestDto request)
+        {
+            if (request == null)
+                return OperationResult<string>.Failure("Request cannot be null.");
+
+            if (request.StartDate > request.EndDate)
+                return OperationResult<string>.Failure("Start date cannot be after end date.");
+
+            if (request.StartDate > DateTime.Now || request.EndDate > DateTime.Now)
+                return OperationResult<string>.Failure("Dates cannot be in the future.");
+
+            return OperationResult<string>.Success("Date range is valid.");
         }
     }
 }
