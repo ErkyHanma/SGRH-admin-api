0267ac1cdd05671921eb816ed40e8a27ddf21f1f
Cris <pachecocruzc258@gmail.com>
2025-07-06 17:46:45 -0400

    Add bussiness logic for Room, Report and Rates services

---
+++ src/SGRH.Application/UseCases/Hotel/Room/RoomMustNotBeOccupied.cs
@@ -0,0 +1,22 @@
+﻿using SGRH.Application.Dtos.Hotel.Room;
+using SGRH.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGRH.Application.UseCases.Hotel.Room
+{
+    public class RoomMustNotBeOccupied
+    {
+        public OperationResult<string> Validate(RoomDto room)
+        {
+            if (room.Status == "occupied")     
+                return OperationResult<string>.Failure("The room is occupied.");
+            
+            return OperationResult<string>.Success("Room is available.");
+
+        }
+    }
+}
