cb17da01e8dbd8724cd94928c60bc45d064f9343
Cris <pachecocruzc258@gmail.com>
2025-07-16 20:41:01 -0400

    Latest

---
+++ src/SGRH.Test.Common/Context/RateContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGRH.Domain.Entities.Hotel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGRH.Common.Test.Context
+{
+    public class RateContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("SGRHConnection");
+        }
+        public DbSet<Rate> Rate { get; set; }
+    }
+}
---
+++ src/SGRH.Test.Common/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SGRH.Test.Common
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
