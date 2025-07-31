d594734890e0b1d4f5b9bd5407ceeac6bba841c4
Cris <pachecocruzc258@gmail.com>
2025-07-30 23:42:48 -0400

    Rates response

---
+++ src/SGRH.Web/Models/Hotel/Rates/Response/GetRateResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace SGRH.Web.Models.Hotel.Rates.Responses
+{
+    public class GetRateResponse : BaseResponse
+    {
+        public RateModel data { get; set; }
+    }
+}
---
+++ src/SGRH.Web/Models/Hotel/Rates/Response/RateEditResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SGRH.Web.Models.Hotel.Rates.Responses
+{
+    public class RateEditResponse : BaseResponse
+    {
+        public RateEditModel data { get; set; } //cuidado
+    }
+
+}
