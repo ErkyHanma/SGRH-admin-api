ccc41723e9752d756e95dbec7baf38262327568e
Cris <pachecocruzc258@gmail.com>
2025-07-30 23:43:08 -0400

    Room response

---
+++ src/SGRH.Web/Models/Hotel/Room/Response/RoomCreateResponse.cs
@@ -0,0 +1,9 @@
+﻿using SGRH.Web.Models.Hotel.Room;
+
+namespace SGRH.Web.Models.Hotel.Room.Responses
+{
+    public class RoomCreateResponse : BaseResponse
+    {
+        public CreateRoomModel data { get; set; }
+    }
+}
